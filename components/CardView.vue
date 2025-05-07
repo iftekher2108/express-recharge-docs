d3d103c3dbc535a0094508b812a6edecdcac1a3b
iftekher2108 <parvezkhan21082002@gmail.com>
2025-05-07 22:37:19 +0600

    quick overview some card added need to more

--- components/CardView.vue
+++ components/CardView.vue
@@ -9,11 +9,11 @@ defineProps({
 <template>
     <div>
         <NuxtLink :to="link">
-            <div class="card glass bg-base-100 hover:bg-primary/30 transition-all duration-300 ease-in-out shadow-xl">
-                <div class="card-body p-4">
+            <div class="card glass bg-base-100 hover:border-2 hover:border-primary/60 lg:min-h-70 min-h-60 hover:bg-primary/30 transition-all duration-300 ease-in-out shadow-xl">
+                <div class="card-body p-3">
                     <i :class="icon"></i>
-                    <h1 class="card-title text-lg uppercase text-primary justify-center">{{ title }}</h1>
-                    <div class="content" v-html="content"></div>
+                    <h1 class="card-title text-lg text-center mt-2 uppercase text-primary justify-center">{{ title }}</h1>
+                    <div class="content text-center text-sm" v-html="content"></div>
                 </div>
             </div>
         </NuxtLink>
@@ -23,6 +23,9 @@ defineProps({
 
 <style lang="scss" scoped>
 .card {
+    border-style: var(--tw-border-style);
+    border-color: rgba(0, 0, 0, 0.311);
+    border-width: 2px;
     .card-body {
         i {
             font-size: 50px;
--- pages/docs/v1_0_0/quick-overview.vue
+++ pages/docs/v1_0_0/quick-overview.vue
@@ -50,28 +50,88 @@ useHead({
         </div>
         <h1 class=" text-3xl font-bold mb-4">Qucik Overview</h1>
 
+        <p>
+            This section highlights the core capabilities and advantages of the web framework used in this project. A
+            modern web framework streamlines development by offering built-in tools, structured architecture, and
+            performance optimizations.
+        </p>
+
+        <br>
+
         <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-3">
 
             <div class="col-span-1">
-                <CardView
-                icon="fa-solid fa-hexagon-nodes-bolt"
-                title="Module"
-                content="
+                <CardView icon="fa-solid fa-hexagon-nodes-bolt" title="Modules" content="
                 <p>
-                    To generate a module-based content framework architecture in Express Recharge, you can create a scalable structure where each module pattern.
+                    To generate a module-based content framework architecture in Express Recharge, create a scalable structure where each module follows a clear pattern for routes, controllers, Middleware, and models organization.
                 </p>
-                "
-                />
-<!-- :link="`/docs/${doc_v}/module`" -->
+                " />
+                <!-- :link="`/docs/${doc_v}/module`" -->
 
             </div>
 
             <div class="col-span-1">
-                <CardView
-                
-                />
+                <CardView icon="fa-solid fa-route" title="Routing" content="
+                <p>
+                    Routing defines how an application’s endpoints respond to client requests, managing control flow and behavior using HTTP methods like GET, POST, PUT, and DELETE for efficient request processing.
+                </p>
+                " />
 
             </div>
+
+            <div class="col-span-1">
+                <CardView icon="fa-solid fa-shield-halved" title="Middleware" content="
+                <p>
+                    Middleware enables request handling logic like authentication, logging, and input validation, ensuring modular, reusable, and secure processing between the client request and server response.
+                </p>
+                " />
+
+            </div>
+
+            <div class="col-span-1">
+                <CardView icon="fa-solid fa-database" title="Database" content="
+                <p>
+                    Database integration offers seamless data management with ORM support, async/await, enabling easy queries, relationships, and dynamic migrations for efficient, secure, and scalable backend data operations.
+                </p>
+                " />
+
+            </div>
+
+            <div class="col-span-1">
+                <CardView icon="fa-solid fa-user-tie" title="Authentication" content="
+                <p>
+                    Authentication and authorization are built-in systems that manage user identity and access control, ensuring only verified users can access specific resources based on defined roles and permissions.
+                </p>
+                " />
+
+            </div>
+
+            <div class="col-span-1">
+                <CardView icon="fa-brands fa-font-awesome" title="Security" content="
+                <p>
+                    Security features include server-side validation, authentication with Bcrypt, ORM for safe database interactions, CLI tools for secure operations, and RESTful API structure for controlled data access.
+                </p>
+                " />
+
+            </div>
+
+            <div class="col-span-1">
+                <CardView icon="fa-solid fa-user-tie" title="Authentication" content="
+                <p>
+                    Authentication and authorization are built-in systems that manage user identity and access control, ensuring only verified users can access specific resources based on defined roles and permissions.
+                </p>
+                " />
+
+                // Example header to link to the icon image
+                app.use((req, res, next) => {
+                res.setHeader('X-Powered-By', 'MyAwesomeLib/1.0.0');
+                res.setHeader('X-Icon', '/static/icon.png'); // Link to local icon
+                next();
+                });
+
+            </div>
+
+
 
         </div>
 
